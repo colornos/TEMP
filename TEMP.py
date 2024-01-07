d6dc785e63e765ccaba20e013a62cb5ae72394dd
colornos <rivas432@hotmail.com>
2024-01-06 19:59:46 -0600

    Update TEMP.py

--- TEMP.py
+++ TEMP.py
@@ -131,17 +131,12 @@ def connect_device(address):
     device_connected = False
     tries = 5
     device = None
-
     while not device_connected and tries > 0:
         try:
             device = adapter.connect(address, 8, addresstype)
             device_connected = True
-        except pygatt.exceptions.NotConnectedError as e:
-            log.error(f"Connection attempt failed: {e}")
+        except pygatt.exceptions.NotConnectedError:
             tries -= 1
-            # Optional: Add a delay here if needed
-            time.sleep(5)  # Delay between retries
-
     return device
 
 def init_ble_mode():
